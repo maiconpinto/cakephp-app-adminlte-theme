a973ca1b7d781c8b39eeb8353c12d89d70a32c0a
Maicon Pinto <maiconsilva.pinto@gmail.com>
2016-10-18 07:17:24 -0200

    Bookmarker tutorial - Creating the Database - BookmarksTagsTable

---
+++ config/Migrations/20161018091707_CreateBookmarksTags.php
@@ -0,0 +1,36 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreateBookmarksTags extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('bookmarks_tags');
+        $table->addColumn('bookmark_id', 'integer', [
+            'default' => null,
+            'limit' => 11,
+            'null' => false,
+        ]);
+        $table->addColumn('tag_id', 'integer', [
+            'default' => null,
+            'limit' => 11,
+            'null' => false,
+        ]);
+        $table->addColumn('created', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('modified', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->create();
+    }
+}
