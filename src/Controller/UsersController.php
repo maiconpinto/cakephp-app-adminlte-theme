c47463fcd6cc2e23176e86de0d6ac689d6110d5a
Maicon Pinto <maiconsilva.pinto@gmail.com>
2016-10-19 12:30:40 -0200

    Bookmarker tutorial - Adding Logout

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -119,4 +119,16 @@ class UsersController extends AppController
             $this->Flash->error('Your username or password is incorrect.');
         }
     }
+    
+    public function initialize()
+    {
+        parent::initialize();
+        $this->Auth->allow(['logout']);
+    }
+
+    public function logout()
+    {
+        $this->Flash->success('You are now logged out.');
+        return $this->redirect($this->Auth->logout());
+    }
 }
