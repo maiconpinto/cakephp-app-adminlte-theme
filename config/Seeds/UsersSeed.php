b022b011f24d7b326d00f2132afc7a7aad753978
Maicon Pinto <maiconsilva.pinto@gmail.com>
2017-01-26 12:09:05 -0200

    Implements Seed

---
+++ config/Seeds/UsersSeed.php
@@ -0,0 +1,29 @@
+<?php
+use Migrations\AbstractSeed;
+
+/**
+ * Users seed.
+ */
+class UsersSeed extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * http://docs.phinx.org/en/latest/seeding.html
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            'email' => 'admin@admin.com',
+            'password' => 'admin'
+        ];
+
+        $table = $this->table('users');
+        $table->insert($data)->save();
+    }
+}
